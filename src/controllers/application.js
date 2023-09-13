d1a0333d53f58a9b4c782737bb378cf1b21bafeb
GurudeepNaik <gurudeepnaik1999@gmail.com>
2023-09-13 16:42:39 +0530

    updated

--- src/controllers/application.js
+++ src/controllers/application.js
@@ -297,6 +297,7 @@ module.exports = {
           JOIN job ON application.applicationjobId = job.jobId
           JOIN profile ON application.applicationuserId = profile.userId
           WHERE application.applicationownerId = ${userId}
+          AND application.rejected != ${0}
           ORDER BY application.${sortBy} ${sortOrder}
           LIMIT ${length} OFFSET ${skip};
         `;
